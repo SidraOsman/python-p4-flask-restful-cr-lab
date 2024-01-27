2f6659ba9eb944abaa3f5c770b13d3584635f8d8
SidraOsman <osmansidra4@gmail.com>
2024-01-27 18:28:43 +0300

    Done with Assignment

--- server/app.py
+++ server/app.py
@@ -17,10 +17,49 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        response_dict_list = [n.to_dict() for n in Plant.query.all()]
+        response = make_response(
+            jsonify(response_dict_list),
+            200,
+        )
+        return response
+    
+    def post(self):
 
+        new_record = Plant(
+            name=request.form['name'],
+            image=request.form['image'],
+            price=request.form['price'],
+        )
+
+        db.session.add(new_record)
+        db.session.commit()
+
+        response_dict = new_record.to_dict()
+
+        response = make_response(
+            jsonify(response_dict),
+            201,
+        )
+
+        return response
+    
 class PlantByID(Resource):
-    pass
+    
+    def get(self, id):
+        response_dict  = Plant.query.filter_by(id=id).first().to_dict()
+
+        response = make_response(
+            jsonify(response_dict),
+            200,
+        )
+        return response
+    
+api.add_resource(PlantByID, '/plants/<int:id>')
+
+api.add_resource(Plants, '/plants')
+
         
 
 if __name__ == '__main__':
--- server/seed.py
+++ server/seed.py
@@ -21,5 +21,43 @@ with app.app_context():
         price=25.98,
     )
 
-    db.session.add_all([aloe, zz_plant])
+    calathea = Plant(
+        id=3,
+        name="Calathea",
+        image="./images/calathea.jpg",
+        price=25.50,
+    )
+
+    fiddle = Plant(
+        id=4,
+        name="fiddle",
+        image="./images/fiddle-leaf-fig.jpg",
+        price= 10.99,
+    )
+    jade = Plant(
+        id=5,
+        name="Jade",
+        image="./images/jade.jpg",
+        price=7.50,
+    )
+    monstera = Plant(
+        id=6,
+        name="Monnstera",
+        image="./images/monstera.jpg",
+        price=30.05,
+    )
+    pilea = Plant(
+        id=7,
+        name="Pilea",
+        image="./images/pilea.jpg",
+        price=15.02,
+    )
+    pothos = Plant(
+        id=8,
+        name="Pothos",
+        image="./images/pothos.jpg",
+        price=17.08,
+    )
+
+    db.session.add_all([aloe, zz_plant, calathea,fiddle,jade,monstera,pilea,pothos])
     db.session.commit()
